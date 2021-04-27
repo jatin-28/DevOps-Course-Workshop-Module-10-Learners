82d9bba7b13ae96fb86b65c71ab925d5ef2d419b
jatin <jatin_28@yahoo.co.uk>
2021-04-27 16:11:36 +0100

    module 10 devops - api

--- WeatherForecast/Controllers/WeatherForecastController.cs
+++ WeatherForecast/Controllers/WeatherForecastController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
+// value: 6pS-kPti98-IB~EJS~~8YFZUg_Ge4n4.B2
+// id: 6a69737a-49c3-4ddc-ac25-f0de24f35732
+//# Replace {tenant} with your tenant!
+//curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'client_id=65832151-79a3-4033-88b6-f78f2e683e04&scope=api%3A%2F%2F9efa84a8-e42e-49c1-8ba6-c7563417d817%2F.default&client_secret=6pS-kPti98-IB~EJS~~8YFZUg_Ge4n4.B2&grant_type=client_credentials' 'https://login.microsoftonline.com/9e39049b-8d44-443e-af95-efab838bd9b9/oauth2/v2.0/token'
 
 namespace webapi.Controllers
 {
     [ApiController]
+    [Authorize(Policy = "ApplicationPolicy")]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
